fdc769ae42ddba9da95b1d4cce356bd6af28f515
BT_1N3_01 <bradyl3@nycstudents.net>
2025-01-10 13:39:51 -0500

    added partner2 Method

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -12,4 +12,8 @@ public class SomeClass {
     public void hello() {
         System.out.println("hello, what's up");
     }
+
+    public void partner2Methods() {
+        System.out.println("yippie!");
+    }
 }
